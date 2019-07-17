b88c9ab1db63b229f026eb89db5a9880413a9900
fvalchar <filip.valchar@ideastatica.com>
2019-07-17 15:14:52 +0200

    icons for social media, marker, background image

--- fival/src/Components/Homepage.tsx
+++ fival/src/Components/Homepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Static/Homepage.css'
+import '../Static/SocialMedia.css'
 
 const Homepage: React.SFC = () => {
     return (
@@ -7,11 +8,15 @@ const Homepage: React.SFC = () => {
             <div className="indent font size-1">
                 * Lorem ipsum dolor sit amet,
                 <br />
-                consectetuer adipiscing elit
+                consectetuer <span className="marker">&nbsp;adipiscing&nbsp;</span> elit
                 <br />
                 malesuada fames ac turpis egestas.
             </div>
             <br />
+            <a href="https://twitter.com/FValch" className="fa fa-twitter" />
+            <a href="https://linkedin.com/in/filip-valchář" className="fa fa-linkedin" />
+            <a href="https://github.com/XuuRee" className="fa fa-github" />
+            <a href="https://www.medium.com/@FilipValchar" className="fa fa-medium" />
             <div className="font size-6">Twitter / Instagram / Goodreads / Medium</div>
             <div className="font size-6">Copyright © 2019 Filip Valchar</div>
         </div>
